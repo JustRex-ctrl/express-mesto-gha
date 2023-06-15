0432ab5680579f698bca2410e844ea8ee2cee139
JustRex-ctrl <ilyavo99@gmail.com>
2023-06-15 13:53:16 +0300

    fix: hotfix2

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./users');
 const cardRoutes = require('./cards');
-const { notFound } = require('../utils/constants');
+const { errNotFound } = require('../utils/constants');
 
 router.use('/users', userRoutes);
 router.use('/cards', cardRoutes);
-router.use('*', (req, res) => res.status(notFound).send({ message: 'Page not found' }));
+router.use('*', (req, res) => res.status(errNotFound).send({ message: 'Page not found' }));
 
 module.exports = router;
