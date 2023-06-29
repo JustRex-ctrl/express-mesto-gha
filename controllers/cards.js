27f01b1c19ef27afddc6ae705d42a8dfd01de09f
JustRex-ctrl <ilyavo99@gmail.com>
2023-06-29 23:04:40 +0300

    feat: fix cards

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,76 +1,58 @@
 const cardSchema = require('../models/card');
-const BadRequestError = require('../errors/BadRequestError');
 const NotFoundError = require('../errors/NotFoundError');
 const ForbiddenError = require('../errors/ForbiddenError');
 
-const getCards = (req, res) => {
+const getCards = (req, res, next) => {
   cardSchema.find({})
     .then((cards) => {
       res.send(cards);
     })
-    .catch(() => res.status(BadRequestError).send({ message: 'error on the server' }));
+    .catch(next);
 };
 
-const postCard = (req, res) => {
+const postCard = (req, res, next) => {
   const { name, link } = req.body;
   cardSchema.create({ name, link, owner: req.user._id })
-    .then((card) => res.send(card))
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        return res.status(ForbiddenError).send({ message: 'Incorrect data when creating card' });
-      }
-      return res.status(BadRequestError).send({ message: 'error on the server' });
-    });
+    .then((card) => res.status(201).send(card))
+    .catch(next);
 };
 
-const deleteCard = (req, res) => {
-  cardSchema.findByIdAndDelete({ _id: req.params.cardId })
+const deleteCard = (req, res, next) => {
+  cardSchema.findOne({ _id: req.params.cardId })
+    .orFail(() => new NotFoundError('This card does not exist'))
     .then((card) => {
-      if (!card) return res.status(NotFoundError).send({ message: 'non-existent cards _id transferred' });
-      return res.send(card);
-    })
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        return res.status(ForbiddenError).send({ message: 'non-existent cards _id transferred' });
+      const cardOwner = card.owner.toString();
+      if (cardOwner !== req.user._id) {
+        throw new ForbiddenError('Card deletion error');
+      } else {
+        const cardId = card._id.toString();
+        cardSchema.deleteOne({ _id: cardId })
+          .then((data) => res.send(data))
+          .catch(next);
       }
-      return res.status(BadRequestError).send({ message: 'error on the server' });
-    });
+    })
+    .catch(next);
 };
 
-const likeCard = (req, res) => {
+const likeCard = (req, res, next) => {
   cardSchema.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } },
     { new: true },
   )
-    .then((card) => {
-      if (!card) return res.status(NotFoundError).send({ message: 'non-existent cards _id transferred' });
-      return res.send(card);
-    })
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        return res.status(ForbiddenError).send({ message: 'Incorrect data sent to like' });
-      }
-      return res.status(BadRequestError).send({ message: 'error on the server' });
-    });
+    .orFail(() => new NotFoundError('Passed non-existent card _id'))
+    .then((card) => res.send(card))
+    .catch(next);
 };
-
-const dislikeCard = (req, res) => {
+const dislikeCard = (req, res, next) => {
   cardSchema.findByIdAndUpdate(
     req.params.cardId,
     { $pull: { likes: req.user._id } },
     { new: true },
   )
-    .then((card) => {
-      if (!card) return res.status(NotFoundError).send({ message: 'non-existent cards _id transferred' });
-      return res.send(card);
-    })
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        return res.status(ForbiddenError).send({ message: 'Incorrect data sent to delete like' });
-      }
-      return res.status(BadRequestError).send({ message: 'error on the server' });
-    });
+    .orFail(() => new NotFoundError('Passed non-existent card _id'))
+    .then((card) => res.send(card))
+    .catch(next);
 };
 
 module.exports = {
@@ -78,5 +60,5 @@ module.exports = {
   postCard,
   deleteCard,
   likeCard,
-  dislikeCard
+  dislikeCard,
 };
