9a95e47729eab0c253e9d65dc7688e9e31cf48f5
JustRex-ctrl <ilyavo99@gmail.com>
2023-07-01 17:18:37 +0300

    fix: hotfix2

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const router = require('./routes/index');
 const handleError = require('./middlewares/handleError');
 const { errors } = require('celebrate');
 const rateLimit = require('express-rate-limit');
+const cookieParser = require('cookie-parser');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
@@ -22,6 +23,7 @@ const limiter = rateLimit({
 app.use(limiter);
 
 app.use(helmet());
+app.use(cookieParser());
 app.use(router);
 app.use(errors());
 app.use(handleError);
