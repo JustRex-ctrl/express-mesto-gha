cc1d2ade62c8a7b22b7e8b72bf1bde108fab3eb5
JustRex-ctrl <ilyavo99@gmail.com>
2023-07-01 19:32:17 +0300

    fix: hotfix9

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,6 @@
 const userSchema = require('../models/user');
-const NotAuthError = require('../errors/NotAuthError'); //401
-const NotFoundError = require('../errors/NotFoundError'); //404
-const MongoDuplicateKeyError = require('../errors/MongoDuplicateKeyError')
+const NotAuthError = require('../errors/NotAuthError');
+const NotFoundError = require('../errors/NotFoundError');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
@@ -12,123 +11,64 @@ const getUsers = (req, res, next) => {
 };
 
 const getUserById = (req, res, next) => {
-  let userId;
-
-  if (req.params.id) {
-    userId = req.params.id;
-  } else {
-    userId = req.user._id;
-  }
-
-  console.log(userId);
-
-  userSchema
-    .findById(userId)
-    .orFail()
+  userSchema.findById(req.params.userId)
+    .orFail(() => new NotFoundError('User by specified _id not found'))
     .then((user) => res.status(200).send(user))
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        return next(new NotFoundError('Invalid data when get user'));
-      }
-
-      if (err.name === 'DocumentNotFoundError') {
-        return next(new NotFoundError(`User Id: ${userId} is not found`));
-      }
-
-      return next(res);
-    });
+    .catch(next);
 };
 
 const createUser = (req, res, next) => {
   const {
-    name,
-    about,
-    avatar,
-    email,
-    password,
+    name, about, avatar, email, password,
   } = req.body;
-
-  bcrypt
-    .hash(password, 10)
-    .then((hash) => userSchema.create({
-      name,
-      about,
-      avatar,
-      email,
-      password: hash,
-    }))
-    .then((user) => {
-      const userObj = user.toObject();
-      delete userObj.password;
-      res.status(201).send(userObj);
+  bcrypt.hash(password, 10)
+    .then((hash) => {
+      userSchema.create({
+        name, about, avatar, email, password: hash,
+      })
+        .then((user) => res.status(201).send(user.deletePassword()))
+        .catch(next);
     })
-    .catch((err) => {
-      if (err.code === 11000) {
-        return next(new MongoDuplicateKeyError('A user with such a email is already registered'));
-      }
-
-      if (err.name === 'ValidationError') {
-        return next(new NotFoundError('Invalid data when post user'));
-      }
-
-      return next(err);
-    });
+    .catch(next);
 };
 
 const updateUser = (req, res, next) => {
   const { name, about } = req.body;
-
-  userSchema.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
+  userSchema.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    { new: true, runValidators: true },)
+    .orFail(() => new NotFoundError('User by specified _id not found'))
     .then((user) => res.send(user))
-    .catch((err) => {
-      if (err.name === 'CastError' || err.name === 'ValidationError') {
-        return next(new NotFoundError('Invalid user id passed'));
-      }
-
-      return next(err);
-    });
+    .catch(next);
 };
 
 const updateAvatar = (req, res, next) => {
-  const { avatar } = req.body;
-
-  userSchema.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
-    .then((user) => res.status(200).send(user))
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        return next(new NotFoundError('Invalid user id passed'));
-      }
-
-      if (err.name === 'DocumentNotFoundError') {
-        return next(new NotFoundError(`User Id: ${req.user._id} is not found`));
-      }
-
-      return next(err);
-    });
+  userSchema.findByIdAndUpdate(
+    req.user._id,
+    { avatar: req.body.avatar },
+    { new: true, runValidators: true },)
+    .then((user) => res.send(user))
+    .catch(next);
 };
 
 const login = (req, res, next) => {
   const { email, password } = req.body;
-
-  return userSchema
-    .findOne({ email })
+  userSchema.findOne({ email })
     .select('+password')
+    .orFail(() => new NotAuthError())
     .then((user) => {
-      if (!user) {
-        return next(new NotAuthError('incorrect email or password'));
-      }
-
-      return bcrypt.compare(password, user.password)
-        .then((matched) => {
-          if (!matched) {
-            return next(new NotAuthError('incorrect email or password'));
+      bcrypt.compare(String(password), user.password)
+        .then((isUserValid) => {
+          if (isUserValid) {
+            const token = jwt.sign({ _id: user._id }, 'secret-key');
+            res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true });
+            res.send(user.deletePassword());
+          } else {
+            throw new NotAuthError();
           }
-
-          const token = jwt.sign({ _id: user._id }, 'secret-person-key', { expiresIn: '7d' });
-          res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true, sameSite: true });
-
-          return res.status(200).send({ token });
-        });
+        })
+        .catch(next);
     })
     .catch(next);
 };
