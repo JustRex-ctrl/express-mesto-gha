e475aedf6c6da042db9e0f21078f0d4151fbd155
JustRex-ctrl <ilyavo99@gmail.com>
2023-07-01 23:38:17 +0300

    fix: hotfix20

--- routes/users.js
+++ routes/users.js
@@ -6,12 +6,11 @@ const {
   getUserById,
   updateUser,
   updateAvatar,
-  getUserById1,
 } = require('../controllers/users');
 const { userIdSchema, avatarSchema, userUpdateSchema } = require('../middlewares/joi-schemas')
 
 router.get('/', getUsers);
-router.get('/me', getUserById1);
+router.get('/me', getUserById);
 router.get('/:userId', celebrate({ params: userIdSchema }), getUserById);
 router.patch('/me', celebrate({ body: userUpdateSchema }), updateUser);
 router.patch('/me/avatar', celebrate({ body: avatarSchema }), updateAvatar);
