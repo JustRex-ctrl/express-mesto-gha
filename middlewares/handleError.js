8d7f5ce1031bb647435f8d5c6a7175488788d90f
JustRex-ctrl <ilyavo99@gmail.com>
2023-07-03 16:17:54 +0300

    fix: test2

--- middlewares/handleError.js
+++ middlewares/handleError.js
@@ -1,10 +1,36 @@
-const handleError = (err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res.status(statusCode)
-    .send({
-      message: statusCode === 500 ? 'Ошибка сервера' : message,
-    });
+const BadRequestError = require('../errors/BadRequestError');
+const MongoDuplicateKeyError = require('../errors/MongoDuplicateKeyError');
+const ForbiddenError = require('../errors/ForbiddenError');
+const InternalError = require('../errors/InternalError');
+const NotFoundError = require('../errors/NotFoundError');
+const NotAuthError = require('../errors/NotAuthError');
+
+function handleError(err, req, res, next) {
+  function setError() {
+    if (err.code === 11000) {
+      return new MongoDuplicateKeyError('This email is already use');
+    }
+    if (err.errors?.email) {
+      return new BadRequestError(`${err.errors.email.value} Wrong email address`);
+    }
+    if (err.name === 'CastError' || err.name === 'ValidationError') {
+      return new BadRequestError();
+    }
+    if (
+      err instanceof NotFoundError
+      || err instanceof NotAuthError
+      || err instanceof BadRequestError
+      || err instanceof ForbiddenError
+    ) {
+      return err;
+    }
+
+    return new InternalError();
+  }
+  const error = setError();
+
+  res.status(error.statusCode).send({ message: error.message, err });
   next();
-};
+}
 
 module.exports = handleError;
