6b5b5f2b241c87c99f379e8ab8fffd10e6647cf8
JustRex-ctrl <ilyavo99@gmail.com>
2023-07-03 14:23:55 +0300

    fix: auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,6 +3,9 @@ const NotAuthError = require('../errors/NotAuthError');
 
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
+  if (!token) {
+    return res.status(NotAuthError).json({message: "Not authorized"})
+}
   let payload;
   try {
     payload = jwt.verify(token, 'secret-key');
